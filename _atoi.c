f52a7759fa447386d0080ffc6f4c223ce1f29df5
Sammynwamama <sammynwamama@gmail.com>
2022-11-16 18:44:16 -0800

    _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -9,9 +9,7 @@
 
 int interactive(info_t *info)
 {
-
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
-
 }
 
 /**
@@ -23,17 +21,11 @@ int interactive(info_t *info)
 
 int is_delim(char c, char *delim)
 {
-
 	while (*delim)
-
 		if (*delim++ == c)
-
 			return (1);
-
 	return (0);
-
 }
-
 
 /**
  *_isalpha - checks for alphabetic character
@@ -43,15 +35,10 @@ int is_delim(char c, char *delim)
 
 int _isalpha(int c)
 {
-
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
 		return (1);
-
 	else
-
 		return (0);
-
 }
 
 /**
@@ -62,49 +49,28 @@ int _isalpha(int c)
 
 int _atoi(char *s)
 {
-
 	int i, sign = 1, flag = 0, output;
-
 	unsigned int result = 0;
 
-
 	for (i = 0;  s[i] != '\0' && flag != 2; i++)
-
 	{
-
 		if (s[i] == '-')
-
 			sign *= -1;
 
-
 		if (s[i] >= '0' && s[i] <= '9')
-
 		{
-
 			flag = 1;
-
 			result *= 10;
-
 			result += (s[i] - '0');
-
 		}
-
 		else if (flag == 1)
-
 			flag = 2;
-
 	}
 
-
 	if (sign == -1)
-
 		output = -result;
-
 	else
-
 		output = result;
 
-
 	return (output);
-
 }
